2be33dd1d602feed84accb704111c12c65ae272a
dong.c <dc111000@hotmail.com>
2019-10-28 18:34:31 +0800

    modify_session_api

--- test03/booktest/urls.py
+++ test03/booktest/urls.py
@@ -16,5 +16,10 @@ urlpatterns = [
     url(r'^login_ajax$',views.login_ajax),   #显示ajax登陆页面
     url(r'^login_ajax_check$',views.login_ajax_check),  #ajax登陆校验
 
+    url(r'^set_cookie$',views.set_cookie),     #设置cookie
+    url(r'^get_cookie$',views.get_cookie),     #获取cookie
 
+    url(r'^set_session$',views.set_session),   #设置session
+    url(r'^get_session$',views.get_session),   #获取session
+    url(r'^clear_session$',views.clear_session),  #清除session
 ]
--- test03/booktest/views.py
+++ test03/booktest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,JsonResponse
 from booktest.models import BookInfo
+from datetime import datetime,timedelta
 
 
 # Create your views here.
@@ -67,3 +68,65 @@ def ajax_handle(request):
     """ajax请求处理"""
     #返回json数据
     return JsonResponse({'res':1})
+
+#/login_ajax
+def login_ajax(request):
+    """显示登陆页面"""
+    return render(request,'booktest/login_ajax.html')
+
+#/login_ajax_check
+def login_ajax_check(request):
+    """ajax登陆校验"""
+    #1、获取用户名和密码
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    #2、进行校验，返回json数据
+    if username == 'smart' and password == '123':
+        #用户名密码正确
+        return JsonResponse({'res':1})
+        #return redirect('/index') ajax请求在后台，不用返回页面或重定向
+    else:
+        #用户名或密码错误
+        return JsonResponse({'res':0})
+
+#/set_ccokies
+def set_cookie(request):
+    """设置cookie信息"""
+    response = HttpResponse("设置cookie")
+    #设置一个cookie信息，名字为num,值为1
+    response.set_cookie('num',1,max_age=14*24*3600)
+    response.set_cookie('num2',2)
+    response.set_cookie('num',1,expires=datetime.now()+timedelta(days=14))
+    #返回response
+    return response
+
+#/get_cookie
+def get_cookie(request):
+    """获取cookie信息"""
+    #取出cookie num的值
+    num = request.COOKIES['num']
+    return HttpResponse(num)
+
+#/set_session
+def set_session(request):
+    """设置session"""
+    request.session['username'] = 'smart'
+    request.session['age'] = 18
+    #设置session有效期
+    #request.session.set_expiry(5)
+    return HttpResponse('设置session')
+
+#/get_session
+def get_session(request):
+    """获取session"""
+    username = request.session['username']   #session不存在会报异常
+    age = request.session['age']
+    return HttpResponse(username+':'+str(age))
+
+#/clear_session
+def clear_session(request):
+    """清除session信息"""
+    #request.session.clear()
+    request.session.flush()
+    return HttpResponse("清除成功")
